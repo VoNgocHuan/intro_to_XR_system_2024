235606c9167b94bd255bcbe2ae7018ca61b285ee
Huan Vo <hvo22@student.oulu.fi>
2024-02-28 17:11:51 +0200

    tried to fixed hand colliders

--- My project/Assets/script/HandPresencePhysics.cs
+++ My project/Assets/script/HandPresencePhysics.cs
@@ -1,21 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class HandPresencePhysics : MonoBehaviour
 {
-    public Transform target;
-    private Rigidbody rb;
-    
-    // Start is called before the first frame update
-    void Start()
+    // Physics Movement
+    [SerializeField] private GameObject followObject;
+    [SerializeField] private float followSpeed = 30f;
+    [SerializeField] private float rotateSpeed = 100f;
+    [SerializeField] private Vector3 positionOffset;
+    [SerializeField] private Vector3 rotationOffset;
+    private Transform _followTarget;
+    private Rigidbody _body;
+
+    private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+
+        // Physics Movement
+        _followTarget = followObject.transform;
+        _body = GetComponent<Rigidbody>();
+        _body.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        _body.interpolation = RigidbodyInterpolation.Interpolate;
+        _body.mass = 20f;
+
+        // Teleport hands
+        _body.position = _followTarget.position;
+        _body.rotation = _followTarget.rotation;
     }
 
-
-    void FixedUpdate()
+    private void Update()
     {
-        rb.velocity = (target.position - transform.position) / Time.fixedDeltaTime;
+        PhysicsMove();
+    }
+
+    private void PhysicsMove()
+    {
+        // Position
+        var positionWithOffset = _followTarget.position + positionOffset;
+        var distance = Vector3.Distance(positionWithOffset, transform.position);
+        _body.velocity = (positionWithOffset - transform.position).normalized * (followSpeed * distance);
+
+        // Rotation
+        var rotationWithOffset = _followTarget.rotation * Quaternion.Euler(rotationOffset);
+        var q = rotationWithOffset * Quaternion.Inverse(_body.rotation);
+        q.ToAngleAxis(out float angle, out Vector3 axis);
+        _body.angularVelocity = axis * (angle * Mathf.Deg2Rad * rotateSpeed);
     }
 }
